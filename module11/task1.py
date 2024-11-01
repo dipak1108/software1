b9f57f2f357d8437cd3f968a46af2e49c90b73b8
dipak <dipakparmar079@gmail.com>
2024-11-01 18:28:22 +0200

    module-11

---
+++ module11/task1.py
@@ -0,0 +1,35 @@
+
+class Publication:
+    def __init__(self, name):
+        self.name = name
+
+class Book(Publication):
+    def __init__(self, name, author, page_count):
+        super().__init__(name)
+        self.author = author
+        self.page_count = page_count
+
+    def print_information(self):
+        print(f"Book Name: {self.name}")
+        print(f"Author: {self.author}")
+        print(f"Page Count: {self.page_count}")
+
+
+class Magazine(Publication):
+    def __init__(self, name, chief_editor):
+        super().__init__(name)
+        self.chief_editor = chief_editor
+
+    def print_information(self):
+        print(f"Magazine Name: {self.name}")
+        print(f"Chief Editor: {self.chief_editor}")
+
+
+if __name__ == "__main__":
+
+    magazine = Magazine("Donald Duck", "Aki Hyyppä")
+    book = Book("Compartment No. 6", "Rosa Liksom", 192)
+
+    magazine.print_information()
+    print()
+    book.print_information()
